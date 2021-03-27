33b2c664f9596e7e5e543afdfa9c934371fdb279
Kayla Melton <kannmelton2@gmail.com>
2021-03-27 16:23:57 -0500

    started working on more complex counter functionality

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -7,6 +7,10 @@ const UseStateCounter = () => {
     setCounterVal(0);
   };
 
+  const complexIncrease = () => {
+    setTimeout(() => {setCounterVal(counterVal + 1)}, 2000)
+  }
+
   return <>
     <section style={{margin: '4rem 0' }}>
       <h2>regular counter</h2>
@@ -14,6 +18,11 @@ const UseStateCounter = () => {
       <button className="btn" onClick={() => setCounterVal(counterVal - 1)}>decrease</button>
       <button className="btn" onClick={reset}>reset</button>
       <button className="btn" onClick={() => setCounterVal(counterVal + 1)}>increase</button>
+    </section>
+    <section style={{margin: '4rem 0' }}>
+      <h2>complex counter</h2>
+      <h1>{counterVal}</h1>
+      <button className="btn" onClick={complexIncrease}>increase later</button>
     </section>
   </>;
 };
